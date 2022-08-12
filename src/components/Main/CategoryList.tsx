e3dd79f03cfb45fe3d56f2fb7208c0d07274668b
publdaze <publdaze@naver.com>
2022-08-12 16:26:12 +0900

    카테고리 목록 컴포넌트 구현하기

---
+++ src/components/Main/CategoryList.tsx
@@ -0,0 +1,34 @@
+import React, { FunctionComponent } from 'react';
+import { Link } from 'gatsby';
+
+export type CategoryListProps = {
+  selectedCategory: string;
+  categoryList: {
+    // 프로퍼티 이름은 문자열, 프로퍼티 값은 숫자임을 나타내는 타입 표기 방법
+    [key: string]: number;
+  };
+};
+
+const CategoryList: FunctionComponent<CategoryListProps> = function ({
+  selectedCategory,
+  categoryList,
+}) {
+  return (
+    // CategoryListWrapper
+    <div className="flex flex-wrap w-[768px] mt-24 m-auto mb-0">
+      {Object.entries(categoryList).map(([name, count]) => (
+        // categoryItem
+        <Link
+          to={`/?category=${name}`}
+          key={name}
+          className={`mr-5 py-1 px-0 text-lg cursor-pointer last-of-type:mr-0
+              ${name === selectedCategory ? 'font-extrabold' : 'font-normal'}`}
+        >
+          #{name}({count})
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default CategoryList;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
 import React, { FunctionComponent } from 'react';
 import '../tailwind.css';
 import Introduction from 'components/Main/Introduction';
+import CategoryList from 'components/Main/CategoryList';
 import Footer from 'components/Common/Footer';
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+};
 
 const IndexPage: FunctionComponent = function () {
   return (
     // Container
     <div className="flex flex-col h-full">
       <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
       <Footer />
     </div>
   );
