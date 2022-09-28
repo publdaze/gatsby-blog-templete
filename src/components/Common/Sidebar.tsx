f1816c7af237b1fb687f842b21ba81b10d872cf4
publdaze <publdaze@naver.com>
2022-09-28 23:43:03 +0900

    상단 바 수정

--- src/components/Common/Sidebar.tsx
+++ src/components/Common/Sidebar.tsx
@@ -19,7 +19,7 @@ type SidebarProps = {
 };
 
 const userNavigation = [
-  { name: 'Your Profile', href: '#' },
+  { name: 'whoami', href: '#' },
   { name: 'Settings', href: '#' },
   { name: 'Sign out', href: '/signIn' },
 ];
@@ -259,7 +259,7 @@ const Sidebar = ({ children }: SidebarProps) => {
           sidebarForDeskOpen ? 'lg:pl-64' : ''
         } flex flex-col flex-1`}
       >
-        <div className="sticky top-0 z-10 flex-shrink-0 flex h-16 bg-white shadow">
+        <div className="sticky top-0 z-10 flex-shrink-0 flex h-12 shadow">
           <button
             type="button"
             className="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 lg:hidden"
@@ -289,42 +289,15 @@ const Sidebar = ({ children }: SidebarProps) => {
             <XIcon className="h-5 w-5" aria-hidden="true" />
           </button>
           <div className="flex-1 px-4 flex justify-between">
-            <div className="flex-1 flex">
-              <form className="w-full flex lg:ml-0" action="#" method="GET">
-                <label htmlFor="search-field" className="sr-only">
-                  Search
-                </label>
-                <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                  <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                    <SearchIcon className="h-5 w-5" aria-hidden="true" />
-                  </div>
-                  <input
-                    id="search-field"
-                    className="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
-                    placeholder="Search"
-                    type="search"
-                    name="search"
-                  />
-                </div>
-              </form>
-            </div>
-            <div className="ml-4 flex items-center lg:ml-6">
-              <button
-                type="button"
-                className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <span className="sr-only">View notifications</span>
-                <BellIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-
+            <div className="flex items-center">
               {/* Profile dropdown */}
-              <Menu as="div" className="ml-3 relative">
+              <Menu as="div" className="mr-2 absolute right-0">
                 <div>
                   <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     <span className="sr-only">Open user menu</span>
                     <img
                       className="h-8 w-8 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      src={profileImg}
                       alt=""
                     />
                   </Menu.Button>
