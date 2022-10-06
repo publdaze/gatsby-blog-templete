9842216e310dbbd4fc5b519adf404c99a9bd49f0
publdaze <publdaze@naver.com>
2022-10-06 22:19:35 +0900

    흰 배경에서 포스팅 제목이 안보이는 문제 해결
    #17

--- src/components/Post/PostHead.tsx
+++ src/components/Post/PostHead.tsx
@@ -18,7 +18,7 @@ const PostHead: FunctionComponent<PostHeadProps> = function ({
       <GatsbyImage
         image={thumbnail}
         alt="thumbnail"
-        className="absolute -z-10 w-full h-72 md:h-96 object-cover brightness-50 "
+        className="absolute -z-10 w-full h-72 md:h-96 object-cover brightness-50 bg-white"
       />
       <PostHeadInfo title={title} date={date} categories={categories} />
     </div>
